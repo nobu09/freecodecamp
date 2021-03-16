d62bcbd272199c268c7717876b4805924edda234
nobu09 <nobu0922@gmail.com>
2021-03-17 08:58:02 +0900

    リセットボタンを押した挙動

--- frontend/projects/25+5clock/index.js
+++ frontend/projects/25+5clock/index.js
@@ -1,20 +1,22 @@
 function PomodoloClock(props) {
   const [breakLen, setBreakLen] = React.useState(5);
   const [sessionLen, setSessionLen] = React.useState(25);
+  const [timeLeft, setTimeLeft] = React.useState("25:00");
 
   const handleClickReset = () => {
     setBreakLen(5);
-    setSessionLen(25);
+    setSessionLen(20);
+    setTimeLeft("20:00");
   }
 
   return (
     <>
-      <LengthControl kind="break" label="Break" default={5} />
-      <LengthControl kind="session" label="Session" default={25} />
+      <LengthControl kind="break" label="Break" length={breakLen} />
+      <LengthControl kind="session" label="Session" length={sessionLen} />
       <div id="timer-label">Session</div>
-      <div id="time-left">25:00</div>
+      <div id="time-left">{timeLeft}</div>
       <button id="start_stop"><i class="fas fa-play-circle"></i></button>
-      <button id="reset"><i class="fas fa-undo"></i></button>
+      <button id="reset" onClick={handleClickReset} ><i class="fas fa-undo"></i></button>
     </>
   )
 }
@@ -26,7 +28,7 @@ function LengthControl(props) {
       <button id={`${props.kind}-increment`} className="btn-level">
         <i class="fas fa-caret-up"></i>
       </button>
-      <div id={`${props.kind}-length`}>{props.default}</div>
+      <div id={`${props.kind}-length`}>{props.length}</div>
       <button id={`${props.kind}-decrement`} className="btn-level ">
         <i class="fas fa-caret-down"></i>
       </button>
