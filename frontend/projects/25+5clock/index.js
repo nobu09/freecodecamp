9a9e0e4e57462d93da5ad45ae2786d01dd56f032
nobu09 <nobu0922@gmail.com>
2021-03-19 08:40:36 +0900

    start_stopボタンを実装し始めた

--- frontend/projects/25+5clock/index.js
+++ frontend/projects/25+5clock/index.js
@@ -2,11 +2,13 @@ function PomodoloClock(props) {
   const [breakLen, setBreakLen] = React.useState(5);
   const [sessionLen, setSessionLen] = React.useState(25);
   const [timeLeft, setTimeLeft] = React.useState("25:00");
+  const [timer, setTimer] = React.useState(false);
 
   const handleClickReset = () => {
     setBreakLen(5);
-    setSessionLen(20);
-    setTimeLeft("20:00");
+    setSessionLen(25);
+    setTimeLeft("25:00");
+    setTimer(false);
   }
 
   return (
@@ -15,8 +17,8 @@ function PomodoloClock(props) {
       <LengthControl kind="session" label="Session" length={sessionLen} />
       <div id="timer-label">Session</div>
       <div id="time-left">{timeLeft}</div>
-      <button id="start_stop"><i class="fas fa-play-circle"></i></button>
-      <button id="reset" onClick={handleClickReset} ><i class="fas fa-undo"></i></button>
+      <button id="start_stop"><i className="fas fa-play-circle"></i></button>
+      <button id="reset" onClick={handleClickReset} ><i className="fas fa-undo"></i></button>
     </>
   )
 }
