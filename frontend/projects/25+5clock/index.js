b9fb0ca071d010059038966ec646b8a44c874cf9
nobu09 <nobu0922@gmail.com>
2021-03-20 19:17:11 +0900

    タイマー機能を実装中

--- frontend/projects/25+5clock/index.js
+++ frontend/projects/25+5clock/index.js
@@ -1,14 +1,30 @@
 function PomodoloClock(props) {
   const [breakLen, setBreakLen] = React.useState(5);
   const [sessionLen, setSessionLen] = React.useState(25);
-  const [timeLeft, setTimeLeft] = React.useState("25:00");
+  const [timeLeft, setTimeLeft] = React.useState(25);
   const [timer, setTimer] = React.useState(false);
+
+  React.useEffect(() => {
+    if (timer) {
+      setInterval(countDown, 1000);
+    }
+  }, [timer]);
+
+  const countDown = () => {
+    setTimeLeft(prevTimeLeft => {
+      return (prevTimeLeft - 1) > 0 ? prevTimeLeft - 1 : sessionLen;
+    });
+  }
 
   const handleClickReset = () => {
     setBreakLen(5);
     setSessionLen(25);
-    setTimeLeft("25:00");
+    setTimeLeft(25);
     setTimer(false);
+  }
+
+  const handleStartStop = () => {
+    setTimer(prevTimer => !prevTimer);
   }
 
   return (
@@ -17,7 +33,7 @@ function PomodoloClock(props) {
       <LengthControl kind="session" label="Session" length={sessionLen} />
       <div id="timer-label">Session</div>
       <div id="time-left">{timeLeft}</div>
-      <button id="start_stop"><i className="fas fa-play-circle"></i></button>
+      <button id="start_stop" onClick={handleStartStop}><i className="fas fa-play-circle"></i></button>
       <button id="reset" onClick={handleClickReset} ><i className="fas fa-undo"></i></button>
     </>
   )
@@ -28,11 +44,11 @@ function LengthControl(props) {
     <div className="length-control">
       <div id={`${props.kind}-label`}>{props.label} length</div>
       <button id={`${props.kind}-increment`} className="btn-level">
-        <i class="fas fa-caret-up"></i>
+        <i className="fas fa-caret-up"></i>
       </button>
       <div id={`${props.kind}-length`}>{props.length}</div>
       <button id={`${props.kind}-decrement`} className="btn-level ">
-        <i class="fas fa-caret-down"></i>
+        <i className="fas fa-caret-down"></i>
       </button>
     </div>
   )
